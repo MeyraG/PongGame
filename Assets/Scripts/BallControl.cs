5f5dc7ae76223010e27b218867fcb4dab31b3418
Meyra <75731320+SuMeyraG@users.noreply.github.com>
2022-04-14 19:31:55 +0300

    fix the ball

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -5,33 +5,47 @@ using UnityEngine;
 public class BallControl : MonoBehaviour
 {
     Rigidbody rb;
+
     [SerializeField]
-    float speed = 5f;
-    //float speeding = 0.8f;
-    //float maxSpeed = 10f;
-
-    //bool move;
-
-
+    float speed = 30f;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         InitialForce();
     }
 
+    float hitFactor(Vector3 ballpos, Vector3 paddlePos, float paddleHeight) 
+    {
+        return (ballpos.z - paddlePos.z) / paddleHeight;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player1")
+        {
+            float z = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.z);
+
+            Vector3 dir = new Vector3(1, 0, z).normalized;
+
+            GetComponent<Rigidbody>().velocity = dir * speed;
+            Debug.Log("ilk if blogu calisti");
+        }
+
+        if (collision.gameObject.name == "PlayerAI")
+        {
+            float z = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.z);
+
+            Vector3 dir = new Vector3(-1, 0, z).normalized;
+
+            GetComponent<Rigidbody>().velocity = dir * speed;
+            Debug.Log("ikinci if blogu calisti");
+        }
+    }
+
     public void InitialForce()
     {
         Vector3 direction = new Vector3(Random.Range(3, 5), 0, 0).normalized;
         rb.AddForce(direction * speed, ForceMode.Impulse);
-    }
-
-    void Update()
-    {
-        //transform.position += transform.right * speed * Time.deltaTime;
-        //speed += speeding * Time.deltaTime;
-        //if (speed > maxSpeed)
-        //{
-        //    speed = maxSpeed;
-        //}
     }
 }
